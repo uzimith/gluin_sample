649b87fa442a327c1d90c01691791a01ec710b37
uzimith <uzimith.x9@gmail.com>
2015-06-02 19:45:46 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function log(text) {
+  console.log(text);
+  $("#console").prepend("<div>"+text+"</div>");
+}
+//----------------------------------------
+// gluin
+//----------------------------------------
